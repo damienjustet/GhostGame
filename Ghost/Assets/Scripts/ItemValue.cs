a9147c0b030f6f38013347309930e7f45e4ec0b9
Griffin Rane <ranegri000@JSD.AD>
2025-11-06 08:35:51 -0700

    Reinstalled Probuilder

---
+++ Ghost/Assets/Scripts/ItemValue.cs
@@ -0,0 +1,22 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.ComponentModel.DataAnnotations;
+// using UnityEngine;
+
+// public class ItemValue : MonoBehaviour
+// {
+//     // public float value;
+//     // [Range(0f, 1f)] public float fragility;
+//     // [Range(0f, 1f)] public float sensitivity;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
---
+++ Ghost/Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems; 
+using Cinemachine;
+using UnityEngine.Networking;
+
+public class Player : MonoBehaviour
+{
+    //references
+    CharacterController rb;
+    public GameObject detect;
+    public float moveSpeed = 5;
+    public CinemachineFreeLook cam;
+    [SerializeField] GameObject player;
+    public Transform target;
+   
+    private void Awake()
+    {
+        rb = GetComponent<CharacterController>();// finds player
+        Cursor.lockState = CursorLockMode.Confined; // confines cursor to window(Might need to click screen to get it to work)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))// Locks Cursor for rotating
+        {
+            Cursor.lockState = CursorLockMode.Locked; 
+            cam.m_XAxis.m_MaxSpeed = 300f;
+            cam.m_YAxis.m_MaxSpeed = 2f;
+        }
+        else // Unlocks Cursor
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            cam.m_XAxis.m_MaxSpeed = 0f;
+            cam.m_YAxis.m_MaxSpeed = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) //For Exiting Play to free Mouse
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        float y_input = Input.GetAxis("Vertical");
+        float x_input = Input.GetAxis("Horizontal");
+        Vector3 cameraForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;// Make Camera forawrd the direction you move
+        Vector3 movement = cameraForward * y_input + Camera.main.transform.right * x_input;//allows strafing
+
+        movement *= moveSpeed;
+
+        rb.SimpleMove(movement); // moves player
+       
+        if (Input.GetKeyDown(KeyCode.E) && !Global.Instance.isPossessed && Global.Instance.interact) // Possession
+        {
+            Global.Instance.isPossessed = true;
+            player.GetComponent<Renderer>().enabled = false;
+            player.GetComponent<Collider>().enabled = false;
+            detect.GetComponent<Renderer>().enabled = false;
+        }
+        
+        else if (Global.Instance.isPossessed == true && Input.GetKeyDown(KeyCode.E)) // Un Possession
+        {
+            player.GetComponent<Renderer>().enabled = true;
+            player.GetComponent<Collider>().enabled = true;
+            detect.GetComponent<Renderer>().enabled = true;
+            Global.Instance.isPossessed = false;
+           
+          
+
+
+        }
+    }
+}
