f03ec05a5d072e07b44e86ced2b79b0c45dd7986
Griffin Rane <ranegri000@JSD.AD>
2025-11-06 09:10:58 -0700

    Trying to get Chace the project

---
+++ Ghost/Assets/Scripts/ItemCost.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCost : MonoBehaviour
+{
+    public float value;
+    [Range(0f, 1f)] public float fragility;
+    [Range(0f, 1f)] public float sensitivity;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
