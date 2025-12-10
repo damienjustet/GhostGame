765b06413ef195c2a806df662589fc60dd7056da
Griffonius <ranegri000@my.jordandistrict.org>
2025-12-10 09:32:26 -0700

    Level 1 in lobby

--- Ghost/Assets/Scripts/Player.cs
+++ Ghost/Assets/Scripts/Player.cs
@@ -26,52 +26,52 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Mouse1))// Locks Cursor for rotating
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            cam.m_XAxis.m_MaxSpeed = 300f;
+            cam.m_YAxis.m_MaxSpeed = 2f;
+        }
+        else // Unlocks Cursor
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            cam.m_XAxis.m_MaxSpeed = 0f;
+            cam.m_YAxis.m_MaxSpeed = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) //For Exiting Play to free Mouse
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        float y_input = Input.GetAxis("Vertical");
+        float x_input = Input.GetAxis("Horizontal");
+        Vector3 cameraForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;// Make Camera forawrd the direction you move
+        Vector3 movement = cameraForward * y_input + Camera.main.transform.right * x_input;//allows strafing
+
+        movement *= moveSpeed;
+
+        if (rib.enabled)
+        {
+            rib.SimpleMove(movement); // moves player
+        }
+        if (Mathf.Abs(y_input) > 0 || Mathf.Abs(x_input) > 0)
+        {
+            // rotates player
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), Mathf.Clamp01(Time.deltaTime * 8));
+
+            // sound effect :)
+            SoundManager.StartSound(SoundType.PLAYERMOVE);
+        }
+        else
+        {
+            
+            SoundManager.StopSound(SoundType.PLAYERMOVE);
+        }
+
         if (LevelLogic.Instance.playerLiving)
         {
-            if (Input.GetKey(KeyCode.Mouse1))// Locks Cursor for rotating
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                cam.m_XAxis.m_MaxSpeed = 300f;
-                cam.m_YAxis.m_MaxSpeed = 2f;
-            }
-            else // Unlocks Cursor
-            {
-                Cursor.lockState = CursorLockMode.Confined;
-                Cursor.visible = true;
-                cam.m_XAxis.m_MaxSpeed = 0f;
-                cam.m_YAxis.m_MaxSpeed = 0f;
-            }
-
-            if (Input.GetKeyDown(KeyCode.Escape)) //For Exiting Play to free Mouse
-            {
-                Cursor.lockState = CursorLockMode.None;
-            }
-
-            float y_input = Input.GetAxis("Vertical");
-            float x_input = Input.GetAxis("Horizontal");
-            Vector3 cameraForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;// Make Camera forawrd the direction you move
-            Vector3 movement = cameraForward * y_input + Camera.main.transform.right * x_input;//allows strafing
-
-            movement *= moveSpeed;
-
-            if (rib.enabled)
-            {
-                rib.SimpleMove(movement); // moves player
-            }
-            if (Mathf.Abs(y_input) > 0 || Mathf.Abs(x_input) > 0)
-            {
-                // rotates player
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), Mathf.Clamp01(Time.deltaTime * 8));
-
-                // sound effect :)
-                SoundManager.StartSound(SoundType.PLAYERMOVE);
-            }
-            else
-            {
-               
-                SoundManager.StopSound(SoundType.PLAYERMOVE);
-            }
-            
             if (Input.GetKeyDown(KeyCode.E) && !LevelLogic.Instance.isPossessed && LevelLogic.Instance.interact) // Possession
             {
                 SoundManager.PlaySound(SoundType.POSSESS);
@@ -83,6 +83,7 @@ public class Player : MonoBehaviour
             
                
             }
+            
         }
     }
 
