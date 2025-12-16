7f5acc222e731b74157088888aef022032c9b3e3
Griffonius <ranegri000@my.jordandistrict.org>
2025-12-16 08:51:40 -0700

    Added arrow

---
+++ Ghost/Assets/Scripts/Arrow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private Renderer rendie;
+    float timer = 0f;
+    Vector3 ogPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rendie = gameObject.GetComponent<SpriteRenderer>();
+        ogPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LevelLogic.Instance.isPossessed && !rendie.enabled)
+        {
+            rendie.enabled = true;
+        }
+        else if (!LevelLogic.Instance.isPossessed && rendie.enabled)
+        {
+            rendie.enabled = false;
+        }
+
+        transform.rotation = Camera.main.transform.rotation;
+
+        timer += Time.deltaTime;
+
+        transform.position = ogPos + Vector3.up * Mathf.Sin(10 * timer) / 10;
+
+    }
+}
