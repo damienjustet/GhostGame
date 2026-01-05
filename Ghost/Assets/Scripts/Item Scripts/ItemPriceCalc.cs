f381bbfa33d11c4031c119ea5f0d6066fbfb4555
Justin <anderjus009@my.jordandistrict.org>
2026-01-05 09:59:04 -0700

    Added A quota calculator

---
+++ Ghost/Assets/Scripts/Item Scripts/ItemPriceCalc.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPriceCalc : MonoBehaviour
+{
+    GameObject[] items;
+    public float totalPrice = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        items = GameObject.FindGameObjectsWithTag("Collectable");
+        for(int i = 0; i < items.Length; i++)
+        {
+            totalPrice += items[i].GetComponent<ItemCost>().value;
+            LevelLogic.Instance.quota = totalPrice * 0.6f;
+            print(totalPrice);
+        }
+
+    }}
+    
+
+
+    // Update is called once per frame
+
