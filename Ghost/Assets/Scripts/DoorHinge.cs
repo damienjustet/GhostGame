84a3d666fec43cad6d6f145743d16c85f1f3f9d7
Griffonius <ranegri000@my.jordandistrict.org>
2026-01-06 07:59:12 -0700

    Simple Door functionality

--- Ghost/Assets/Scripts/DoorHinge.cs
+++ Ghost/Assets/Scripts/DoorHinge.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class DoorHinge : MonoBehaviour
 {
-    GameObject hingeCenter;
-    void Start()
-    {
-        
-    }
+    [Range(-1,1)] public int direction;
+    bool isOpen = false;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (isOpen)
+            {
+                CloseDoor();
+            }
+            else
+            {
+                OpenDoor();
+            }
+        }
+    }
+
+    void OpenDoor()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.y += 90 * direction;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+
+    void CloseDoor()
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.y -= 90 * direction;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
     }
 }
